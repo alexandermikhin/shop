b41ff09c4cbef440265748ffab95c4c450f25733
Aliaksandr Mikhin <Aliaksandr_Mikhin@epam.com>
2019-03-28 10:08:21 +0300

    feat (shop): update product component with data
    Update product component with mock data.

--- shop/src/app/product/product.component.ts
+++ shop/src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from '../models/category';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  name: string;
+  description: string;
+  price: number;
+  category: Category;
+  isAvailable: boolean;
+  suppliers: string[];
+
   constructor() { }
 
   ngOnInit() {
+    this.name = 'Product name';
+    this.description = 'Product description';
+    this.price = 100;
+    this.category = Category.category1;
+    this.isAvailable = true;
+    this.suppliers = [
+      'Supplier 1',
+      'Supplier 2',
+      'Supplier 3'
+    ];
   }
-
 }
