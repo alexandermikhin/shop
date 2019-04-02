d5def6bee1f2424ba33d0b08a37fb5a911770571
Aliaksandr Mikhin <Aliaksandr_Mikhin@epam.com>
2019-04-02 22:07:56 +0300

    feat (shop): create modules for sections
    Create modules for sections Cart, Products, Orders, Core, Shared.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { CartModule } from './cart/cart.module';
 import { CartComponent } from './components/cart/cart.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductComponent } from './components/product/product.component';
+import { CoreModule } from './core/core.module';
+import { OrdersModule } from './orders/orders.module';
+import { ProductsModule } from './products/products.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -13,9 +18,14 @@ import { ProductComponent } from './components/product/product.component';
     CartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CartModule,
+    ProductsModule,
+    OrdersModule,
+    CoreModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
