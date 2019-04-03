1b1018f91948fc1d5e1fb4a4ace45c79263a6dc7
Aliaksandr Mikhin <Aliaksandr_Mikhin@epam.com>
2019-04-03 21:28:34 +0300

    feat (shop): add onpush strategy to the cart item component
    Add OnPush change detection strategy to the CartItemComponent.

--- src/app/cart/components/cart-item/cart-item.component.ts
+++ src/app/cart/components/cart-item/cart-item.component.ts
@@ -1,10 +1,17 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output
+} from '@angular/core';
 import { CartItem } from '../../models/cart-item.model';
 
 @Component({
   selector: 'app-cart-item',
   templateUrl: './cart-item.component.html',
-  styleUrls: ['./cart-item.component.css']
+  styleUrls: ['./cart-item.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CartItemComponent {
   @Input() item: CartItem;
