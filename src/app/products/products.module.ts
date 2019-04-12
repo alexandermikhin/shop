81c3771b150e6b274fc0e430dc6da208a66581b2
Aliaksandr Mikhin <Aliaksandr_Mikhin@epam.com>
2019-04-12 09:42:29 +0300

    chore (shop): remove extra service module declaration
    Remove ProductsService declaration in ProductsModule as it is duplicate.

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductComponent } from './components/product/product.component';
-import { ProductsService } from './services/products.service';
 
 @NgModule({
   declarations: [ProductComponent, ProductListComponent],
   imports: [CommonModule, SharedModule],
-  providers: [ProductsService],
   exports: [ProductListComponent]
 })
 export class ProductsModule {}
