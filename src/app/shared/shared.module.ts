73c5169e77687fbf60e0068587f33e0c9f467270
Aliaksandr Mikhin <Aliaksandr_Mikhin@epam.com>
2019-04-12 09:35:38 +0300

    chore (shop): make common declarations and exported directives
    In SharedModule make common declared and exported directives.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 import { HighlightDirective } from './directives/highlight.directive';
 import { BorderDirective } from './directives/border.directive';
 
+const exportedDeclarations = [HighlightDirective, BorderDirective];
+
 @NgModule({
   imports: [CommonModule],
-  declarations: [HighlightDirective, BorderDirective],
-  exports: [HighlightDirective, BorderDirective]
+  declarations: [...exportedDeclarations],
+  exports: [...exportedDeclarations]
 })
 export class SharedModule {}
