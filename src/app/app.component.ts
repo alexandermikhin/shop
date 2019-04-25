a59845a99c0f70f0d5c459a8392e84ca2ece6161
Aliaksandr Mikhin <Aliaksandr_Mikhin@epam.com>
2019-04-25 23:07:31 +0300

    feat (core): use app settings service to load settings
    Use AppSettings service to load service either from local storage,
    server or set default.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AppSettingsService } from './core/services/app-settings.service';
+import { AppSettings } from './core/models/app-settings';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit, OnDestroy {
+  settings: AppSettings;
+  private settingsSub: Subscription;
+  constructor(private appSettingsService: AppSettingsService) {
+
+  }
+
+  ngOnInit() {
+    this.settingsSub = this.appSettingsService.loadSettings().subscribe(s => this.settings = s);
+  }
+
+  ngOnDestroy() {
+    if (this.settingsSub) {
+      this.settingsSub.unsubscribe();
+    }
+  }
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,12 +4,16 @@ import { ConstantsService } from './services/constants.service';
 import { Generator15, generatorFactory } from './services/generator-factory';
 import { GeneratorService } from './services/generator.service';
 import { constantsService } from './models/constants';
+import { AppSettingsService } from './services/app-settings.service';
+import { LocalStorageService } from './services/local-storage.service';
 
 
 @NgModule({
   declarations: [],
   providers: [
+    AppSettingsService,
     GeneratorService,
+    LocalStorageService,
     { provide: ConstantsService, useValue: constantsService},
     {
       provide: Generator15,
