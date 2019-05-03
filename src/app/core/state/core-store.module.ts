77e59317ad7dcf2720dd6bece4aca0b9a39646d4
Aliaksandr Mikhin <Aliaksandr_Mikhin@epam.com>
2019-05-03 05:44:38 +0300

    feat (shop): add router state
    Add router state.

--- src/app/core/state/core-store.module.ts
+++ src/app/core/state/core-store.module.ts
@@ -1,16 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from 'src/environments/environment';
+import {
+  routerReducers,
+  RouterStateSerializerProvider
+} from './router/router.state';
 
 @NgModule({
   imports: [
     CommonModule,
-    StoreModule.forRoot({}),
+    StoreModule.forRoot(routerReducers),
+    StoreRouterConnectingModule.forRoot(),
     EffectsModule.forRoot([]),
     !environment.production ? StoreDevtoolsModule.instrument() : []
-  ]
+  ],
+  providers: [RouterStateSerializerProvider]
 })
 export class CoreStoreModule {}
