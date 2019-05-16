20a46b7ad43dfd9030bb4106d14ad97c1e86134c
Aliaksandr Mikhin <Aliaksandr_Mikhin@epam.com>
2019-05-16 21:12:49 +0300

    test (cart): fix tests with missing imports
    Fix tests with missing import.

--- src/app/cart/components/cart-list/cart-list.component.spec.ts
+++ src/app/cart/components/cart-list/cart-list.component.spec.ts
@@ -1,17 +1,32 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
+import { DialogService } from 'src/app/core/services/dialog.service';
+import { LocalStorageService } from 'src/app/core/services/local-storage.service';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { CartListComponent } from './cart-list.component';
 
 describe('CartComponent', () => {
   let component: CartListComponent;
   let fixture: ComponentFixture<CartListComponent>;
+  let localStorageService: jasmine.SpyObj<LocalStorageService>;
+  let dialogService: jasmine.SpyObj<DialogService>;
+
+  beforeEach(() => {
+    localStorageService = jasmine.createSpyObj('LocalStorageService', [
+      'getItem'
+    ]);
+    dialogService = jasmine.createSpyObj('DialogService', ['confirm']);
+  });
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [SharedModule, FormsModule],
       declarations: [CartListComponent],
+      providers: [
+        { provide: LocalStorageService, useValue: localStorageService },
+        { provide: DialogService, useValue: dialogService }
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   }));
--- src/app/cart/services/cart.service.spec.ts
+++ src/app/cart/services/cart.service.spec.ts
@@ -1,9 +1,22 @@
 import { TestBed } from '@angular/core/testing';
-
+import { LocalStorageService } from 'src/app/core/services/local-storage.service';
 import { CartService } from './cart.service';
 
 describe('CartService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let localStorageService: jasmine.SpyObj<LocalStorageService>;
+  beforeEach(() => {
+    localStorageService = jasmine.createSpyObj('LocalStorageService', [
+      'getItem'
+    ]);
+  });
+
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: LocalStorageService, useValue: localStorageService }
+      ]
+    })
+  );
 
   it('should be created', () => {
     const service: CartService = TestBed.get(CartService);
