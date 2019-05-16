ddd9ad709d2f0f6b87d32cd0955f179dd7e8ba6f
Aliaksandr Mikhin <Aliaksandr_Mikhin@epam.com>
2019-05-16 20:15:05 +0300

    test (orders): add tests for order by pipe
    Add tests for order by pipe.

--- src/app/shared/pipes/order-by.pipe.spec.ts
+++ src/app/shared/pipes/order-by.pipe.spec.ts
@@ -1,5 +1,5 @@
-import { OrderByPipe } from './order-by.pipe';
 import { CartItem } from 'src/app/cart/models/cart-item.model';
+import { OrderByPipe } from './order-by.pipe';
 
 describe('OrderByPipe', () => {
   let pipe: OrderByPipe;
@@ -13,13 +13,15 @@ describe('OrderByPipe', () => {
     cartItems = [
       { name: 'item-1', price: 1, quantity: 1 },
       { name: 'ITEM-3', price: 1, quantity: 3 },
+      { name: 'item-1', price: 1, quantity: 1 },
       { name: 'item-2', price: 1, quantity: 2 }
     ];
 
     const expected: CartItem[] = [
       { name: 'item-1', price: 1, quantity: 1 },
+      { name: 'item-1', price: 1, quantity: 1 },
       { name: 'item-2', price: 1, quantity: 2 },
-      { name: 'ITEM-3', price: 1, quantity: 3 },
+      { name: 'ITEM-3', price: 1, quantity: 3 }
     ];
 
     const actual = pipe.transform(cartItems);
@@ -37,7 +39,7 @@ describe('OrderByPipe', () => {
     const expected: CartItem[] = [
       { name: 'item-2', price: 2, quantity: 2 },
       { name: 'ITEM-3', price: 3, quantity: 3 },
-      { name: 'item-1', price: 5, quantity: 1 },
+      { name: 'item-1', price: 5, quantity: 1 }
     ];
 
     const actual = pipe.transform(cartItems, 'price');
@@ -91,10 +93,46 @@ describe('OrderByPipe', () => {
     const expected: CartItem[] = [
       { name: 'item', price: 30, quantity: 20 },
       { name: 'expensive-few', price: 500, quantity: 1 },
-      { name: 'cheap-much', price: 2, quantity: 200 },
+      { name: 'cheap-much', price: 2, quantity: 200 }
     ];
 
     const actual = pipe.transform(cartItems, 'sum', true);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('should preserve original order if unknown sort option', () => {
+    cartItems = [
+      { name: 'item-1', price: 5, quantity: 1 },
+      { name: 'ITEM-3', price: 3, quantity: 3 },
+      { name: 'item-2', price: 2, quantity: 2 }
+    ];
+
+    const expected: CartItem[] = [
+      { name: 'item-1', price: 5, quantity: 1 },
+      { name: 'ITEM-3', price: 3, quantity: 3 },
+      { name: 'item-2', price: 2, quantity: 2 }
+    ];
+
+    const actual = pipe.transform(cartItems, 'date' as any);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('should sort by name by default', () => {
+    cartItems = [
+      { name: 'item-1', price: 5, quantity: 1 },
+      { name: 'ITEM-3', price: 3, quantity: 3 },
+      { name: 'item-2', price: 2, quantity: 2 }
+    ];
+
+    const expected: CartItem[] = [
+      { name: 'item-1', price: 5, quantity: 1 },
+      { name: 'item-2', price: 2, quantity: 2 },
+      { name: 'ITEM-3', price: 3, quantity: 3 }
+    ];
+
+    const actual = pipe.transform(cartItems);
 
     expect(actual).toEqual(expected);
   });
